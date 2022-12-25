dc827d8599707c37bc9eb4c8da5408695585b353
Stryukov <stryukov.semen@gmail.com>
2022-12-25 14:13:58 +1200

    Added static and models

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Post
+from .models import Group
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
+    search_fields = ('text',)
+    list_filter = ('pub_date', 'group',)
+    empty_value_display = '-пусто-'
+    list_editable = ('group',)
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
